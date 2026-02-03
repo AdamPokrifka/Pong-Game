23588d1cf22299066ca5d3023b005534c3a2c196
AdamPokrifka <adampokrifka@gmail.com>
2026-02-03 13:26:48 -0500

    Add files via upload

---
+++ pong 2 player/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong 2 player
+{
+    public partial class Form1 : Form
+    {
+        //Declare variables
+        Rectangle paddle;
+        Rectangle paddle2;
+        Rectangle ball;
+
+
+        int paddleSpeed = 20;
+        int paddle2Speed = 20;
+        int ballX = 4;
+        int ballY = 4;
+
+        //Socre
+        int score = 0;
+
+        Timer gameTimer = new Timer();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Setup Form
+            this.Width = 600;
+            this.Height = 400;
+            this.Text = "This is my new title.";
+            this.BackColor = Color.Black;
+            this.DoubleBuffered = true;
+
+            //Create a Rectangular Paddle
+            paddle = new Rectangle(this.ClientSize.Width / 2 - 40, this.ClientSize.Height - 20, 80, 10);
+
+            //Create a Rectangular Paddle2
+            paddle2 = new Rectangle(this.ClientSize.Width / 2 - 40, this.ClientSize.Height - 20, 80, 10);
+
+            //Create a Ball
+            ball = new Rectangle(this.ClientSize.Width / 2, this.ClientSize.Height / 2, 15, 15);
+
+            //Timer
+            gameTimer.Interval = 20; // Update 50 times/second
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+
+            //Paint Event - Draws paddle and ball
+            this.Paint += DrawGame;
+
+
+        }
+        private void GameLoop(object sender, EventArgs e)
+        {
+            //Move the ball
+            ball.X += ballX;
+            ball.Y += ballY;
+
+            //bounce off left/right walls
+            if (ball.Left <= 0 || ball.Right >= this.ClientSize.Width)
+                ballX = -ballX;
+
+            //bounce off top
+            if (ball.Top <= 0)
+                ballY = -ballY;
+
+            //CheckBox paddle collison
+            if (ball.IntersectsWith(paddle))
+            {
+                ballY = -ballY;
+                score++;
+                this.Text = "Simple Pong Version 1 | Score: " + score;
+            }
+
+            
+            //CheckBox paddle2 collison
+            if (ball.IntersectsWith(paddle2))
+            {
+                ballY = -ballY;
+                score++;
+                this.Text = "Simple Pong Version 1 | Score: " + score;
+            }
+
+
+            //miss ball
+            if (ball.Bottom >= this.ClientSize.Height)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Game Over. Try Again. Final Score: " + score);
+            }
+            if (ball.Top >= this.ClientSize.Height)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Game Over. Try Again. Final Score: " + score);
+            }
+
+            Invalidate();
+
+
+        }
+        private void DrawGame(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.AliceBlue, paddle);
+
+            e.Graphics.FillRectangle(Brushes.AliceBlue, paddle2);
+
+            e.Graphics.FillEllipse(Brushes.Green, ball);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left && paddle.Left > 0)
+                paddle.X -= paddleSpeed;
+            else if (keyData == Keys.Right && paddle.Right > 0)
+                paddle.X += paddleSpeed;
+
+           
+
+             if (keyData == Keys.A && paddle2.Left > 0)
+                paddle2.X -= paddle2Speed;
+            else if (keyData == Keys.D && paddle2.Right > 0)
+                paddle2.X += paddle2Speed;
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+    }
+}
+
---
+++ pong 2 player/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pong 2 player")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DOS")]
+[assembly: AssemblyProduct("pong 2 player")]
+[assembly: AssemblyCopyright("Copyright © DOS 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2428f557-e500-46c5-a8cd-987948260001")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
